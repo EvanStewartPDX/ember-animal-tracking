9469fd64f2cfc677d7216eead091f6d7b6661172
Timothy Herold <heroldt@gmail.com>
2016-11-07 13:54:02 -0800

    Fix the build.

--- app/services/location-service.js
+++ app/services/location-service.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   latlng: null,
-  getLatLngFromZip(zipcode, fn) {
+  getLatLngFromZip(address, fn) {
     var geocoder = new window.google.maps.Geocoder();
     var output;
-    geocoder.geocode( { 'address': `'${zipcode}'`}, function(results, status) {
+    geocoder.geocode( { 'address': `'${address}'`}, function(results, status) {
       if (status === 'OK') {
         output = {
          lat: results[0].geometry.location.lat(),
@@ -19,28 +19,22 @@ export default Ember.Service.extend({
       }
     });
   },
-  addMap(container) {
-    var lat = this.get('lat');
-    var lng = this.get('lng');
+  addMap(container, lat, lng) {
     if(lat && lng) {
-
-      var container =  this.$('.map-canvas')[0];
       var options = {
-          center: new window.google.maps.LatLng(
-            this.get('lat'),
-            this.get('lng')
-          ),
+          center: new window.google.maps.LatLng(lat, lng),
           mapTypeId: 'satellite',
           zoom: 15
       };
-      console.log(`Lat: ${lat}, Lng: ${lng}`);
-      var map = new window.google.maps.Map(container, options);
-      new window.google.maps.Marker({
-        position: {lat: lat, lng: lng},
-        map: map,
-        title: 'test'
-      });
+      return new window.google.maps.Map(container, options);
     }
+  },
+  addMarker(map, lat, lng, title) {
+    return new window.google.maps.Marker({
+      position: {lat: lat, lng: lng},
+      map: map,
+      title: title
+    });
   }
 
 });
