bf4e481aa4604c4dc7a0dc43a5fb0145dc487e40
Timothy Herold <heroldt@gmail.com>
2016-11-07 14:54:46 -0800

    Add ability to set markers for location.

--- app/services/location-service.js
+++ app/services/location-service.js
@@ -14,11 +14,10 @@ export default Ember.Service.extend({
         if(fn) {
           fn(output);
         }
-      } else {
-        //handle error
       }
     });
   },
+
   addMap(container, lat, lng) {
     if(lat && lng) {
       var options = {
@@ -29,11 +28,31 @@ export default Ember.Service.extend({
       return new window.google.maps.Map(container, options);
     }
   },
+
   addMarker(map, lat, lng, title) {
-    return new window.google.maps.Marker({
-      position: {lat: lat, lng: lng},
-      map: map,
-      title: title
+    var google = window.google;
+    var marker = new window.google.maps.Marker({
+      position: { lat: lat, lng: lng },
+      map: map
+    });
+    marker['infowindow'] = title;
+    var infowindow = new google.maps.InfoWindow({});
+    google.maps.event.addListener(marker , 'click', function(){
+      infowindow.setContent(this['infowindow']);
+      infowindow.open(map, this);
+    });
+  },
+
+  getResults(map, lat, lng) {
+    var service = this;
+    var taxa = 'Aves';
+    return Ember.$.get('http://api.inaturalist.org/v1/observations?iconic_taxa='+ taxa +'&lat='+ lat +'&lng='+ lng  +'&updated_since=2016&order=desc&order_by=created_at').then(data => {
+      data.results.forEach(result => {
+        var location = result.location.split(',');
+        if(location) {
+          service.addMarker(map, parseFloat(location[0]), parseFloat(location[1]), 'test');
+        }
+      });
     });
   }
 
