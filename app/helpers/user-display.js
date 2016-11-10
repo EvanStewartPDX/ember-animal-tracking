cae4e5601b6e1c7ae1ea7375683d32476e1fa887
Laura Krouse and Molly LeCompte <molly@>
2016-11-10 08:30:16 -0800

    fix user-tile positioning.

--- app/helpers/user-display.js
+++ app/helpers/user-display.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export function userDisplay(params) {
   var userAuthentication = Ember.inject.service();
-  var user = params[0];
+
 
   if (userAuthentication.currentUser) {
      return Ember.String.htmlSafe('<div class="username"> {{userAuthentication.username}}</div> <img class="tileimg" src={{userAuthentication.avatar}}>');
